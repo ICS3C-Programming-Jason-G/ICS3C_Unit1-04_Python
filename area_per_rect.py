6c5f261cbda2608b987b60883ed5ceeb79a49dd6
Jason Grace <jason.grace@stu.ocsb.ca>
2023-10-05 16:20:49 +0000

    Added area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created by: Jason Grace
+# Created on: October 4th, 2023
+# This program calculates the area & perimeter of a 15.3cm X 29.1cm rectangle.
+
+
+def main():
+    print("For a rectangle with length 29.1cm and width 15.3cm")
+    print("")
+    print("The area is {:.1f}cm^2".format(29.1 * 15.3))
+    print("The perimeter is {:.1f}cm".format(2 * (29.1 + 15.3)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Jason Grace
+# Date: October 2nd, 2023
+# This program does basic arithmetic math.
+
+
+def main():
+    print("10-5={}".format(10 - 5))
+    print("2+4={}".format(2 + 4))
+    print("8*2={}".format(8 * 2))
+    print("6/3={}".format(6 / 3))
+    print("1/4={}".format(1 / 4))
+    print("2+5/3**2*5={}".format(2 + 5 / 3**2 * 5))
+    print("2^3={}".format(2 ^ 3))  # bitwise operator
+    print("2**3={}".format(2**3))  # exponent
+    print("4**0.5={}".format(4**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
